4750483279fafc0819c8147d7ad70e7bc6979a9f
zhoxing-ms <Zhou.Xing@microsoft.com>
2024-09-03 18:11:32 +0800

    Add the logic to call CLI handler

--- src/copilotRequestHandler.ts
+++ src/copilotRequestHandler.ts
@@ -1,17 +1,49 @@
 import * as vscode from 'vscode';
 
-const MODEL_SELECTOR: vscode.LanguageModelChatSelector = { vendor: 'copilot', family: 'gpt-4' };
+interface ResponseData {  
+    success: boolean;  
+    message: string;  
+}  
+
+type Dictionary = Record<string, string>;
+
+async function sendPostRequest(url: string, auth_token: string, data: Dictionary): Promise<ResponseData> {  
+    try {  
+        const response = await fetch(url, {  
+            method: 'POST',  
+            headers: {  
+                'Content-Type': 'application/json',
+                "Authorization": auth_token  
+            },  
+            body: JSON.stringify(data),  
+        });  
+  
+        if (!response.ok) {  
+            throw new Error(`HTTP error! status: ${response.status}`);  
+        }  
+  
+        const responseData: ResponseData = await response.json() as ResponseData;  
+
+        return responseData;  
+          
+    } catch (error) {  
+        console.error('Unexpected error:', error);  
+        throw error;  
+    }  
+}  
+
+const endpointUrl = 'https://azclitools-copilot-apim-test.azure-api.net/azcli/copilot';
+const auth_token = 'xxxxxxxx'
 
 export async function copilotRequestHandler(request: vscode.ChatRequest, context: vscode.ChatContext, stream: vscode.ChatResponseStream, token: vscode.CancellationToken) {
     
     // TODO: Implement request our handling logic here
-    const messages = [
-        vscode.LanguageModelChatMessage.User(`You are an assistant who generates corresponding Azure CLI command combinations based on user question.`),
-        vscode.LanguageModelChatMessage.User(request.prompt)
-    ];
-    const [model] = await vscode.lm.selectChatModels(MODEL_SELECTOR);
-    const chatResponse = await model.sendRequest(messages, {}, token);
-    for await (const fragment of chatResponse.text) {
+    const postData: Dictionary = {  
+        "question": request.prompt 
+    };
+
+    const chatResponse = await sendPostRequest(endpointUrl, auth_token, postData);
+    for await (const fragment of chatResponse.message) {
         stream.markdown(fragment);
     }
 
